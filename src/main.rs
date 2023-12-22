fb29026907c21cbda132821a28a7c168d2e5aa56
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-22 15:47:23 +0000

    Added movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use tcod::console::Root;
+use tcod::colors::*;
+use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
-const SCREEN_WIDTH: u32 = 80;
-const SCREEN_HEIGHT: u32 = 50;
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
 
 const LIMIT_FPS: i32 = 20;
 
@@ -10,5 +13,39 @@ struct Tcod {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("assets/arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let mut game = Tcod { root };
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !game.root.window_closed() {
+        game.root.set_default_foreground(WHITE);
+        game.root.clear();
+        game.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
+        game.root.flush();
+        if handle_keys(&mut game, &mut player_x, &mut player_y) {
+            break;
+        }
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {}
+    }
+    false
 }
