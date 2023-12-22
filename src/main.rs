a31070b7785744647c221e709fcc7da794f7233c
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-22 05:01:12 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use tcod::console::Root;
+
+const SCREEN_WIDTH: u32 = 80;
+const SCREEN_HEIGHT: u32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod {
+    root: Root,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
